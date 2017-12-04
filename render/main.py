aabd4f840ad1f5877ed090c7fd1d29b01e434848
dkeriazisStuy <dkeriazis@stuy.edu>
2017-12-03 21:49:40 -0500

    Updated render and setup.py

--- render/main.py
+++ render/main.py
@@ -1,9 +1,22 @@
-def init():
+import cgi
+import cgitb
+
+
+def init(debug=False):
     print('Content-Type: text/html\n')
     print()
+    if debug:
+        cgitb.enable()
 
 
 def render_file(filename):
     with open(filename) as f:
         text = f.read()
     print(text)
+
+
+def make_dict(field):
+    field_dict = {}
+    for i in field.keys():
+        field_dict[i] = field[i].value
+    return field_dict

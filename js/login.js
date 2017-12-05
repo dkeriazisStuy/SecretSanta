d5eb32d76d82b985771895452a2e7da78ec82471
dkeriazisStuy <dkeriazis@stuy.edu>
2017-12-04 23:20:55 -0500

    Debugging login

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+function get_hash(s, salt) {
+    result = hash(hash(s + salt) + s + salt);
+    for (i = 0; i < 1000; i++) {
+        result = hash(result + s + salt);
+    }
+    return result + " " + salt;
+}
+
 function login() {
     var username = $("#username").val();
     var password = $("#password").val();
@@ -9,14 +17,11 @@ function login() {
             users = Object.keys(o);
             if (username in o) {
                 var nonce = o[username]["nonce"];
-                var h = hash(password + nonce);
                 var check = o[username]["pass"];
                 var salt = check.split(" ")[1];
-                var result = hash(hash(h + salt) + h + salt);
-                for (i = 0; i < 1000; i++){
-                    result = hash(result + h + salt);
-                }
-                result += ' ' + salt;
+                var result = get_hash(hash(password + nonce), salt);
+                console.log(check);
+                console.log(result);
                 // Check to protect from length extension attacks
                 if (check.length != result.length) {
                     return false;

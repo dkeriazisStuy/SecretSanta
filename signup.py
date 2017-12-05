82c00e77c80602af7e0f0151cc0d22ae2be1b36b
dkeriazisStuy <dkeriazis@stuy.edu>
2017-12-04 20:00:14 -0500

    Rough version of signup

--- signup.py
+++ signup.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 import render
+from util import add_user
+
 render.init()
+
+
+def main():
+    fields = render.get_fields()
+    try:
+        username = fields['username']
+        email = fields['email']
+        key = fields['key']
+        nonce = fields['nonce']
+    except KeyError:
+        return  # TODO: Render "something went wrong" page
+    add_user(username, email, key, nonce)
+    print("Account created")
+
+
+main()

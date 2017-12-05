5ade663d0b9eee675fb6c954d9b6d119ac40f147
dkeriazisStuy <dkeriazis@stuy.edu>
2017-12-04 22:16:22 -0500

    Added cookie support

--- js/common.js
+++ js/common.js
@@ -21,3 +21,26 @@ function post(path, params, method) {
     document.body.appendChild(form);
     form.submit();
 }
+
+function setCookie(name, value, millis) {
+    var d = new Date();
+    d.setTime(d.getTime() + millis);
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    var name = name + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}

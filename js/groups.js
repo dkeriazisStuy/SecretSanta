e922071db1d052ff751a57e080c42c189304ce1d
dkeriazisStuy <dkeriazis@stuy.edu>
2017-12-09 00:58:32 -0500

    Group name now requires printable ascii characters

--- js/groups.js
+++ js/groups.js
@@ -1,4 +1,7 @@
 function create_group() {
+    if checkName() {
+        return;
+    }
     var group_name = $("#name").val();
     var group_description = $("#description").val();
     f_success = function(username, series_id, new_token) {
@@ -13,6 +16,21 @@ function create_group() {
     }
     checkCookies(f_success, f_fail);
 }
+
+function checkName() {
+    var name = $("#name").val();
+    if (! (/^[\x20-\x7E]+$/.test(name))) {
+        $("#invalidName").html("Name must be one or more printable ascii characters");
+        return false;
+    } else {
+        $("#invalidName").html("");
+        return true;
+    }
+}
+
+$(document).ready(function () {
+    $("#name").keyup(checkName);
+});
 
 document.getElementById("addGroup").addEventListener("click", function() {
     create_group();
